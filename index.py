5e23e274197bfe4db648d23a8fadd8dc96826f4b
Joey Peterson <joey.peterson15@gmail.com>
2024-10-30 11:19:07 -0700

    gitignore and load model

--- index.py
+++ index.py
@@ -4,13 +4,14 @@ import torch.utils.data.dataloader
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-import kaggle
-
-
+import torch.nn as nn
 
 batch_size = 100
-input_size = 48 * 48 # 48x48
+input_size = 96 * 96 # 96x96
 learning_rate = 0.001
+epochs = 2
+classes = ('airplane', 'bird', 'car', 'cat', 'deer', 'dog', 'horse', 'monkey', 'ship', 'truck')
+output_size = len(classes)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -21,16 +22,62 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 )
 
-# train_data = torchvision.datasets.Flowers102(root='./data', split='train', transform=transform, download=True)
-train_data = torchvision.datasets.CelebA(root='data/', split='train', download=True, transform=transform)
-test_data = torchvision.datasets.Flowers102(root='./data', split='test', transform=transform, download=True)
-train_loader = torch.utils.data.dataloader(train_data, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.dataloader(test_data, batch_size=batch_size, shuffle=False)
+train_data = torchvision.datasets.STL10(root='./data', split='train', download=True, transform=transform)
+test_data = torchvision.datasets.STL10(root='./data', split='test', transform=transform, download=True)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
-print('train_loader', train_loader.size())
-classes = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
+# images, labels = next(iter(train_loader))
+# print(images)
+# print(labels)
 
-class ConvFerNet(torch.nn.Module):
+# print('train_loader', train_data)
+kernel_size1 = 5 #filter size
+# kernel_size2 = 5
+class ConvFerNet(nn.Module):
     def __init__(self):
         super(ConvFerNet, self).__init__()
-        self.convL1 = torch.nn.Conv2d(1)
+        # conv output = ((W - F) + 2P)/S + 1 F=filter_size, W=inputSize, P=padding, S=stride
+        self.convL1 = torch.nn.Conv2d(3, 6, kernel_size1)
+        self.pool = torch.nn.MaxPool2d(2, 2) #cuts inputs in half
+        self.convL2 = torch.nn.Conv2d(6, 16, kernel_size1)
+        self.linL1 = torch.nn.Linear(16 * 21 * 21, 1200)
+        self.linL2 = torch.nn.Linear(1200, 400)
+        self.linL3 = torch.nn.Linear(400, 84)
+        self.linL4 = torch.nn.Linear(84, output_size)
+        self.relu = torch.nn.ReLU()
+    
+    def forward(self, x):
+        x = self.pool(self.relu(self.convL1(x)))
+        x = self.pool(self.relu(self.convL2(x)))
+        x = x.view(-1, 16 * 21 * 21)
+        x = self.relu(self.linL1(x))
+        x = self.relu(self.linL2(x))
+        x = self.relu(self.linL3(x))
+        x = self.linL4(x)
+        return x
+
+model = ConvFerNet().to(device)
+
+# loss and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+total_steps = len(train_loader)
+# train model
+for epoch in range(epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # images, labels = train_loader[i]
+        images = images.to(device)
+        labels = labels.to(device)
+        output = model(images) # forward pass
+        loss = criterion(output, labels) #calculate loss
+
+        optimizer.zero_grad() #zero out last gradient
+        loss.backward() # calculate descent gradient for each weight
+        optimizer.step() # update weights
+
+        if (i + 1) % 1000 == 0:
+            print(f'step: {i}/{total_steps}, loss: {loss.item():.4f}')
+
+
+
